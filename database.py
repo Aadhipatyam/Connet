5f0a53b5636f17cce38745c249b27d8a331e51a4
Aadhipatyam <piyushraiiem72@gmail.com>
2025-05-22 11:40:08 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Use SQLite for simplicity. Replace with PostgreSQL URI for production
 DATABASE_URL = "sqlite:///./products.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
